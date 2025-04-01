f1db62ac111086f2787fa3df3a41c188b5eafd49
Marcin Bator <marcinbator.ofc@gmail.com>
2025-04-01 18:33:24 +0200

    feat: #1 image without gesture filtration

---
+++ scripts/common/get_learning_files.py
@@ -0,0 +1,24 @@
+import os
+
+folder_path = os.path.abspath("../../../bdgs_photos")
+
+
+def get_learning_files():
+    image_files = []
+    classify_file = None
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.lower().endswith(".txt"):
+                classify_file = os.path.join(root, file)
+                break
+
+        if classify_file is None or len(files) == 0: continue
+
+        with open(classify_file, "r") as f:
+            classify_table = [line.split(" ")[0] for line in f]
+        files.pop(0)  # remove classify file
+
+        for index in range(len(files) - 1):
+            if files[index].lower().endswith(('.png', '.jpg', '.jpeg')) and classify_table[index] != "0":
+                image_files.append((os.path.join(root, files[index]), classify_table[index]))
+    return image_files
--- scripts/murthy_jadon/murthy_jadon_test.py
+++ scripts/murthy_jadon/murthy_jadon_test.py
@@ -5,15 +5,17 @@ import cv2
 from bdgs import recognize
 from bdgs.algorithms.murthy_jadon.murthy_jadon import MurthyJadon
 from bdgs.classifier import ALGORITHM
+from scripts.common.get_learning_files import get_learning_files
 
-folder_path = "../resources"
-
-image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+folder_path = os.path.abspath("../../../bdgs_photos")
 
 
 def test_process_image():
-    for image_file in image_files:
-        image_path = os.path.join(folder_path, image_file)
+    images = get_learning_files()
+
+    for image_file in images:
+        image_path = os.path.join(folder_path, image_file[0])
+        print(image_path)
         image = cv2.imread(image_path)
 
         if image is not None:
@@ -34,16 +36,16 @@ def test_process_image():
             print(f"Failed to load image: {image_file}")
 
 
-def test_classify():
-    for image_file in image_files:
-        image_path = os.path.join(folder_path, image_file)
-        image = cv2.imread(image_path)
-
-        if image is not None:
-            result = recognize(image, algorithm=ALGORITHM.MURTHY_JADON)
-            print(f"Result for {image_file}: {result}")
-        else:
-            print(f"Failed to load image: {image_file}")
+# def test_classify():
+#     for image_file in image_files:
+#         image_path = os.path.join(folder_path, image_file)
+#         image = cv2.imread(image_path)
+# 
+#         if image is not None:
+#             result = recognize(image, algorithm=ALGORITHM.MURTHY_JADON)
+#             print(f"Result for {image_file}: {result}")
+#         else:
+#             print(f"Failed to load image: {image_file}")
 
 
 test_process_image()
