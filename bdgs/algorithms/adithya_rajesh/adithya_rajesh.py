24050e9e0fcbe23bcd07e037bb9811a3985067cf
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-04-04 18:19:14 +0200

    Refactor: #3 Adapted algorithm to payload class architecture

--- bdgs/algorithms/adithya_rajesh/adithya_rajesh.py
+++ bdgs/algorithms/adithya_rajesh/adithya_rajesh.py
@@ -2,11 +2,13 @@ import numpy as np
 import cv2
 
 from bdgs.algorithms.bdgs_algorithm import BaseAlgorithm
+from bdgs.models.image_payload import ImagePayload
 from bdgs.gesture import GESTURE
 
 
 class AdithyaRajesh(BaseAlgorithm):
-    def process_image(self, image: np.ndarray) -> np.ndarray:
+    def process_image(self, payload: ImagePayload) -> np.ndarray:
+        image = payload.image
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = cv2.resize(image, (100, 100))
         image = image.astype(np.float32)
