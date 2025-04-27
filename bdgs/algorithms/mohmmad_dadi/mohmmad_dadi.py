3559b1e2719703355ca511ff0ed33c4ac67a8b33
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2025-04-27 18:52:40 +0200

    feat: #24 process_image first approach

--- bdgs/algorithms/mohmmad_dadi/mohmmad_dadi.py
+++ bdgs/algorithms/mohmmad_dadi/mohmmad_dadi.py
@@ -1,14 +1,28 @@
+import cv2
 import numpy as np
 
 from bdgs.algorithms.bdgs_algorithm import BaseAlgorithm
 from bdgs.data.processing_method import PROCESSING_METHOD
 from bdgs.models.image_payload import ImagePayload
+from bdgs.data.gesture import GESTURE
 
 
 class MohmmadDadi(BaseAlgorithm):
     def process_image(self, payload: ImagePayload,
                       processing_method: PROCESSING_METHOD = PROCESSING_METHOD.DEFAULT) -> np.ndarray:
-        pass
+        # Experimental
+        if processing_method != PROCESSING_METHOD.DEFAULT:
+            from bdgs.data.algorithm_functions import ALGORITHM_FUNCTIONS
+            return ALGORITHM_FUNCTIONS[processing_method].process_image(payload)
+
+        gray = cv2.cvtColor(payload.image, cv2.COLOR_BGR2GRAY)
+        _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+        kernel = np.ones((3, 3), np.uint8)
+        eroded = cv2.erode(binary, kernel, iterations=2)
+        dilated = cv2.dilate(eroded, kernel, iterations=1)
+        processed = cv2.subtract(binary, dilated)
+        edges = cv2.Canny(processed, threshold1=100, threshold2=200)
+        return edges
 
     def classify(self, payload: ImagePayload,
                  processing_method: PROCESSING_METHOD = PROCESSING_METHOD.DEFAULT) -> (GESTURE, int):
